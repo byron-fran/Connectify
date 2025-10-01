6e5b9020971dac5e303d4e30f40848e3de435562
byron <1byron1999@gmail.com>
2025-10-01 15:42:56 -0700

    Add Contact UseCases

---
+++ app/src/main/java/com/example/connectify/domain/useCases/ContactUseCases.kt
@@ -0,0 +1,58 @@
+package com.example.connectify.domain.useCases
+
+import com.example.connectify.domain.models.Contact
+import com.example.connectify.domain.repositories.ContactRepository
+import kotlinx.coroutines.flow.Flow
+
+class InsertContactUseCase(private val contactRepository: ContactRepository) {
+
+    suspend operator fun invoke(contact: Contact) {
+        contactRepository.insertContact(contact)
+    }
+
+}
+
+class GetAllContactsUseCase(
+    private val contactRepository: ContactRepository
+) {
+    operator fun invoke(): Flow<List<Contact>> {
+        return contactRepository.getAllContacts()
+
+    }
+
+}
+
+class GetContactById(
+    private val contactRepository: ContactRepository
+) {
+    operator fun invoke(id: String): Flow<Contact?> {
+        return contactRepository.getContactById(id)
+    }
+
+}
+
+
+class UpdateContact(
+    private val contactRepository: ContactRepository
+) {
+    suspend operator fun invoke(contact: Contact) {
+        contactRepository.insertContact(contact)
+    }
+}
+
+
+class DeleteContact(
+    private val contactRepository: ContactRepository
+) {
+    suspend operator fun invoke(contact: Contact) {
+        contactRepository.deleteContactById(contact)
+    }
+}
+
+data class ContactUseCases (
+    val insertContact: InsertContactUseCase,
+    val getAllContacts: GetAllContactsUseCase,
+    val getContactById: GetContactById,
+    val updateContact: UpdateContact,
+    val deleteContact: DeleteContact
+)
