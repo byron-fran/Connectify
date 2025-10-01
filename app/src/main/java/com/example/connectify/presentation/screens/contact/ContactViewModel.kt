3f1c3daae4d745ecbedbdc315f4a54cf7a5d5fee
byron <1byron1999@gmail.com>
2025-10-01 16:27:19 -0700

    Add ContactViewModel

---
+++ app/src/main/java/com/example/connectify/presentation/screens/contact/ContactViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.connectify.presentation.screens.contact
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.connectify.domain.models.Contact
+import com.example.connectify.domain.useCases.ContactUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ContactViewModel @Inject constructor(
+    private val contactUseCases: ContactUseCases
+) : ViewModel() {
+
+    private val _contactState = MutableStateFlow(ContactState())
+    val contactState = _contactState.asStateFlow()
+
+    init {
+        getAllContacts()
+    }
+
+    fun insertContact(contact : Contact) {
+        viewModelScope.launch {
+          contactUseCases.insertContact(contact)
+        }
+    }
+
+
+    fun getAllContacts() {
+        viewModelScope.launch {
+            contactUseCases.getAllContacts().collect {
+                _contactState.update { state ->
+                    state.copy(
+                        contacts = it
+                    )
+                }
+            }
+        }
+
+    }
+
+
+    fun getContactById(id : String) {
+        viewModelScope.launch {
+            contactUseCases.getContactById(id).collect {
+                _contactState.update { state ->
+                    state.copy(
+                        contact = it
+                    )
+                }
+            }
+        }
+    }
+
+    fun updateContact(contact : Contact) {
+        viewModelScope.launch {
+            contactUseCases.updateContact(contact)
+        }
+    }
+
+    fun deleteContact(contact : Contact) {
+        viewModelScope.launch {
+            contactUseCases.deleteContact(contact)
+        }
+
+    }
+}
