cc7f5137de87421c1eead72b2e2cefd3746e3e5d
byron <1byron1999@gmail.com>
2025-10-01 15:28:38 -0700

    Add Repository

---
+++ app/src/main/java/com/example/connectify/domain/repositories/ContactRepository.kt
@@ -0,0 +1,18 @@
+package com.example.connectify.domain.repositories
+
+import com.example.connectify.domain.models.Contact
+import kotlinx.coroutines.flow.Flow
+
+interface ContactRepository {
+
+    suspend fun insertContact (contact : Contact)
+
+    fun getAllContacts() : Flow<List<Contact>>
+
+    fun getContactById(id : String) : Flow<Contact?>
+
+    suspend fun deleteContactById(contact: Contact)
+
+    suspend fun updateContact(contact: Contact)
+
+}
