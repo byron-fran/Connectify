a7ee04785de7e4c5aad7c09bcc5b71042d9ddb19
byron <1byron1999@gmail.com>
2025-10-12 03:54:55 -0700

    Add Card Utilities

---
+++ app/src/main/java/com/example/connectify/ui/theme/Card.kt
@@ -0,0 +1,13 @@
+package com.example.connectify.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Card {
+    val card_xs = 24.dp
+    val card_sm = 32.dp
+    val card_md = 48.dp
+    val card_lg = 64.dp
+    val card_xl = 96.dp
+    val card_xxl = 128.dp
+    val card_3xl = 160.dp
+}
