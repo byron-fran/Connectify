5e115ebafe326808f4e304a1c6cbdbdad1b344d2
byron <1byron1999@gmail.com>
2025-10-14 21:57:55 -0700

    Add Basic Icons component

---
+++ app/src/main/java/com/example/connectify/presentation/components/global/Icons.kt
@@ -0,0 +1,56 @@
+package com.example.connectify.presentation.components.global
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import com.example.connectify.ui.theme.Card
+
+
+@Composable
+fun CustomIcon(
+    @DrawableRes icon: Int,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.primary,
+    size: Dp =Card.card_xs,
+) {
+
+    Icon(
+        painter = painterResource(id = icon),
+        contentDescription = null,
+        modifier = modifier.size(size),
+        tint = color
+    )
+
+}
+
+@Composable
+fun CustomIconButton(
+    @DrawableRes icon: Int,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.primary,
+    size: Dp = Card.card_xs,
+    onClick: () -> Unit,
+) {
+    IconButton(
+        onClick = { onClick() },
+        colors = IconButtonDefaults.iconButtonColors(
+            contentColor = color
+        ),
+        modifier = modifier.size(size)
+    ) {
+
+        Icon(
+            painter = painterResource(id = icon),
+            contentDescription = null,
+        )
+    }
+
+}
