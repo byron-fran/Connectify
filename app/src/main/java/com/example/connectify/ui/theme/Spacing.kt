96fa64c0fd33737c8e449fd7bfcd4a031cabce10
byron <1byron1999@gmail.com>
2025-10-14 22:13:41 -0700

    Add Spacings

---
+++ app/src/main/java/com/example/connectify/ui/theme/Spacing.kt
@@ -0,0 +1,11 @@
+package com.example.connectify.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val spacing_xs = 2.dp
+    val spacing_sm = 4.dp
+    val spacing_md = 8.dp
+    val spacing_lg = 16.dp
+    val spacing_xl = 24.dp
+}
