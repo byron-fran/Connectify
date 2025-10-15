666870975563e27286338b42d1a08244546c4890
byron <1byron1999@gmail.com>
2025-10-14 22:14:23 -0700

    Add Custom corners

---
+++ app/src/main/java/com/example/connectify/ui/theme/RoundedCorner.kt
@@ -0,0 +1,11 @@
+package com.example.connectify.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object RoundedCorner {
+    val rounded_corner_xs = 2.dp
+    val rounded_corner_sm = 4.dp
+    val rounded_corner_md  = 8.dp
+    val rounded_corner_lg = 16.dp
+    val rounded_corner_xl = 24.dp
+}
