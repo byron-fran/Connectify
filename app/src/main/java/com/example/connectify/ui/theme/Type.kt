5533cfa32d34e9831f882005b63a20e0975e193a
byron <1byron1999@gmail.com>
2025-10-14 22:50:32 -0700

    Enhance typography styles with custom font families for titles and body text

--- app/src/main/java/com/example/connectify/ui/theme/Type.kt
+++ app/src/main/java/com/example/connectify/ui/theme/Type.kt
@@ -3,28 +3,63 @@ package com.example.connectify.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.example.connectify.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(
+            Font(R.font.roboto_extra_bold)
+        ),
+        fontWeight = FontWeight.Normal,
+        fontSize = 25.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily(
+            Font(R.font.roboto_extra_bold)
+        ),
+        fontWeight = FontWeight.Normal,
+        fontSize = 21.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = FontFamily(
+            Font(R.font.roboto_medium)
+        ),
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = FontFamily(
+            Font(R.font.roboto_regular)
+        ),
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily(
+            Font(R.font.roboto_light)
+        ),
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    // Other default text styles to override
+
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
@@ -32,16 +67,4 @@ val Typography = Typography(
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
-
-@Composable
-fun Card() {
-}
-
-@Preview
-@Composable
-fun CardPreview() {
-
-    Card()
-}
