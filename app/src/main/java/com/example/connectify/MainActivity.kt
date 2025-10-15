84fbe3fa6fc83d048ca0662a0a714e8a19bdc5d1
byron <1byron1999@gmail.com>
2025-10-14 22:50:01 -0700

    Replace Scaffold with NavHostManager for improved navigation handling in MainActivity

--- app/src/main/java/com/example/connectify/MainActivity.kt
+++ app/src/main/java/com/example/connectify/MainActivity.kt
@@ -4,34 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import com.example.connectify.presentation.navigation.NavHostManager
 import com.example.connectify.ui.theme.ConnectifyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ConnectifyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        "Hola a todos",
-                        color = Color.Green,
-                        fontSize = 33.sp,
-                        modifier = Modifier
-                            .padding(innerPadding)
-                            .background(Color.Red)
-                    )
-                }
+                NavHostManager()
             }
         }
     }
