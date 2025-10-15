21c7ce81ef84eae299b29d97a40f3ca38adad2eb
byron <1byron1999@gmail.com>
2025-10-14 23:09:15 -0700

    Add dark and light color themes in Color.kt

--- app/src/main/java/com/example/connectify/ui/theme/Color.kt
+++ app/src/main/java/com/example/connectify/ui/theme/Color.kt
@@ -2,10 +2,20 @@ package com.example.connectify.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val DarkBackground = Color(0xFF000000)
+val DarkOnBackground = Color(0xFFFFFFFF)
+val DarkPrimary = Color(0xFF4F39F6)
+val DarkOnPrimary = Color(0xFFFFFFFF)
+val DarkSecondary = Color(0x334F39F6)
+val DarkOnSecondary = Color(0xFF4f39f6)
+val DarkSurface = Color(0xFF2E2E2E)
+val DarkOnSurface = Color(0xFFD9D9D9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightBackground = Color(0xFFFFFFFF)
+val LightOnBackground = Color(0xFF000000)
+val LightPrimary = Color(0xFF4f39f6)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightSecondary = Color(0x334F39F6)
+val LightOnSecondary = Color(0xFF4f39f6)
+val LightSurface = Color (0xFFE9E8E8)
+val LightOnSurface = Color(0xFF2E2E2E)
