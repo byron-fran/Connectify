8e8819830e9ed12b954b68c29d9a5b4734980df2
byron <1byron1999@gmail.com>
2025-11-09 08:01:17 -0800

    Refactor ContactForm to improve text field handling and encapsulate image logic in ContactFormImage component

--- app/src/main/java/com/example/connectify/presentation/components/contact/ContactForm.kt
+++ app/src/main/java/com/example/connectify/presentation/components/contact/ContactForm.kt
@@ -9,20 +9,22 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import com.example.connectify.R
 import com.example.connectify.presentation.components.global.ButtonPrimary
 import com.example.connectify.presentation.components.global.CustomIcon
 import com.example.connectify.presentation.components.global.CustomIconButton
-import com.example.connectify.presentation.components.global.CustomImage
+import com.example.connectify.presentation.components.global.LabelMedium
+import com.example.connectify.presentation.components.global.ContactImage
 import com.example.connectify.ui.theme.Card
 import com.example.connectify.ui.theme.RoundedCorner
 import com.example.connectify.ui.theme.Spacing
@@ -33,14 +35,11 @@ fun ContactForm(
     name: String,
     phoneNumber: String,
     email: String,
-    imageUri: String?,
     isButtonEnabled: Boolean,
     modifier: Modifier = Modifier,
     onNameChange: (String) -> Unit,
     onPhoneNumberChange: (String) -> Unit,
     onEmailChange: (String) -> Unit,
-    onImageChange: () -> Unit,
-    onClearImage: () -> Unit,
     onSubmit: () -> Unit
 ) {
 
@@ -48,72 +47,96 @@ fun ContactForm(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(Spacing.spacing_lg)
     ) {
-        TextField(
+
+        ContactTextField(
             value = name,
-            onValueChange = { newName ->
-                onNameChange(newName)
-            },
-            label = { Text(stringResource(R.string.name)) },
-            colors = customTextFieldColors(),
-            shape = RoundedCornerShape(RoundedCorner.rounded_corner_md),
-            modifier = Modifier.fillMaxWidth()
+            onValueChange = { onNameChange(it) },
+            label = stringResource(R.string.name)
         )
-        TextField(
+
+        ContactTextField(
             value = phoneNumber,
-            onValueChange = { newNumber ->
-                onPhoneNumberChange(newNumber)
-            },
-            label = { Text(stringResource(R.string.phone_number)) },
-            colors = customTextFieldColors(),
-            shape = RoundedCornerShape(RoundedCorner.rounded_corner_md),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-            modifier = Modifier.fillMaxWidth()
+            onValueChange = onPhoneNumberChange,
+            label = stringResource(R.string.phone_number),
+            keyboardType = KeyboardType.Number
+
         )
-        TextField(
+        ContactTextField(
             value = email,
-            onValueChange = { newEmail ->
-                onEmailChange(newEmail)
-            },
-            label = { Text(stringResource(R.string.email)) },
-            colors = customTextFieldColors(),
-            shape = RoundedCornerShape(RoundedCorner.rounded_corner_md),
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-            modifier = Modifier.fillMaxWidth()
+            onValueChange = onEmailChange,
+            label = stringResource(R.string.email),
+            keyboardType = KeyboardType.Email
         )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(Card.card_xxl)
-                    .clickable { onImageChange() }
-            ) {
-                if (imageUri.isNullOrEmpty()) {
-                    CustomIcon(
-                        icon = R.drawable.icon_image,
-                        modifier = Modifier.fillMaxSize(),
-                        color = MaterialTheme.colorScheme.surface
-                    )
-                } else {
-                    CustomImage(imageUri, modifier = Modifier.fillMaxSize())
-                }
-            }
-            if (imageUri?.isNotEmpty() == true) {
-                CustomIconButton(
-                    icon = R.drawable.icon_close,
-                    color = MaterialTheme.colorScheme.surface
-                ) {
-                    onClearImage()
-                }
-            }
-        }
         ButtonPrimary(
             text = stringResource(R.string.save),
             modifier = Modifier.fillMaxWidth(),
             enabled = isButtonEnabled,
         ) {
             onSubmit()
+        }
+    }
+}
+
+
+@Composable
+fun ContactTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    label: String,
+    modifier: Modifier = Modifier,
+    keyboardType: KeyboardType = KeyboardType.Text
+) {
+
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { LabelMedium(label) },
+        colors = customTextFieldColors(),
+        shape = RoundedCornerShape(RoundedCorner.rounded_corner_md),
+        keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
+        textStyle = MaterialTheme.typography.bodyLarge,
+        modifier = modifier.fillMaxWidth()
+    )
+
+}
+
+@Composable
+fun ContactFormImage(
+    imageUri: String?,
+    onImageChange: () -> Unit,
+    onClearImage: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .size(Card.card_3xl)
+                .clip(CircleShape)
+                .clickable { onImageChange() },
+
+            ) {
+
+            ContactImage(imageUri, modifier = Modifier.fillMaxSize()) {
+                CustomIcon(
+                    icon = R.drawable.icon_image,
+                    color = MaterialTheme.colorScheme.surface,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(CircleShape)
+                )
+            }
+        }
+        imageUri?.let {
+            CustomIconButton(
+                icon = R.drawable.icon_close,
+                color = MaterialTheme.colorScheme.surface,
+            ) {
+                onClearImage()
+            }
         }
     }
 }
