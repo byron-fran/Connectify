de5b191c35b16c72094228966a52bcc3ebe813a2
byronfran <1byron1999@gmail.com>
2025-11-15 19:54:05 -0800

    Add error color definitions for Dark and Light themes

--- app/src/main/java/com/example/connectify/ui/theme/Color.kt
+++ app/src/main/java/com/example/connectify/ui/theme/Color.kt
@@ -1,21 +1,25 @@
 package com.example.connectify.ui.theme
 
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
 
 val DarkBackground = Color(0xFF000000)
 val DarkOnBackground = Color(0xFFFFFFFF)
 val DarkPrimary = Color(0xFF4F39F6)
 val DarkOnPrimary = Color(0xFFFFFFFF)
-val DarkSecondary = Color(0x334F39F6)
+val DarkSecondary = Color(0x404F39F6)
 val DarkOnSecondary = Color(0xFF4f39f6)
 val DarkSurface = Color(0xFF2E2E2E)
 val DarkOnSurface = Color(0xFFD9D9D9)
-
+val DarkError = Color(0x3BFF0000)
+val DarkOnError = Color(0xFFC51414)
 val LightBackground = Color(0xFFFFFFFF)
 val LightOnBackground = Color(0xFF000000)
 val LightPrimary = Color(0xFF4f39f6)
 val LightOnPrimary = Color(0xFFFFFFFF)
-val LightSecondary = Color(0x334F39F6)
+val LightSecondary = Color(0x264F39F6)
 val LightOnSecondary = Color(0xFF4f39f6)
 val LightSurface = Color (0xFFE9E8E8)
 val LightOnSurface = Color(0xFF2E2E2E)
+val LightError = Color(0x34F61A1A)
+val LightOnError = Color(0xFFD91010)
