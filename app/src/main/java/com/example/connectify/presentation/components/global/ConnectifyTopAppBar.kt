9fc2695c97ee47c886e3475542058010c230cc2a
byronfran <1byron1999@gmail.com>
2025-11-15 19:55:19 -0800

    Add error color definitions for Dark and Light themes

--- app/src/main/java/com/example/connectify/presentation/components/global/ConnectifyTopAppBar.kt
+++ app/src/main/java/com/example/connectify/presentation/components/global/ConnectifyTopAppBar.kt
@@ -1,6 +1,7 @@
 package com.example.connectify.presentation.components.global
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -15,6 +16,7 @@ import com.example.connectify.ui.theme.Card
 fun ConnectifyToAppBar(
     title: String,
     canNavigateBack: Boolean = true,
+    actions: @Composable (RowScope.() -> Unit) = {},
     onNavigate: () -> Unit
 ) {
 
@@ -25,7 +27,7 @@ fun ConnectifyToAppBar(
             }
         },
         navigationIcon = {
-            if (canNavigateBack){
+            if (canNavigateBack) {
                 CustomIconButton(
                     icon = R.drawable.icon_arrow_back,
                     color = MaterialTheme.colorScheme.onBackground,
@@ -37,6 +39,7 @@ fun ConnectifyToAppBar(
         },
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.background
-        )
+        ),
+        actions = actions
     )
 }
