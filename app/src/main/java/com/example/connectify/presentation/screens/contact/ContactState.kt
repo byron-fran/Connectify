15d70c77c8ccb27ad5080c7ab7f8e4562ae10eab
byron-fran <1byron1999@gmail.com>
2025-11-21 15:24:01 -0800

    Add selectedContact, loading, and error fields to ContactState

--- app/src/main/java/com/example/connectify/presentation/screens/contact/ContactState.kt
+++ app/src/main/java/com/example/connectify/presentation/screens/contact/ContactState.kt
@@ -4,5 +4,8 @@ import com.example.connectify.domain.models.Contact
 
 data class ContactState(
     val contact : Contact? = null,
-    val contacts : List<Contact> = emptyList()
+    val contacts : List<Contact> = emptyList(),
+    val selectedContact: Contact? = null,
+    val loading: Boolean = false,
+    val error: String? = null
 )
