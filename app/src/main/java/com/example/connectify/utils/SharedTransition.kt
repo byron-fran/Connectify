f5abffe8ead4e8dbe6f14c2af804d54196096211
byron-fran <1byron1999@gmail.com>
2025-11-26 06:05:55 -0800

    Refactor: Add optional screenKey to shared transition keys
    The `sharedTransitionImageKey` and `sharedTransitionTitleKey` methods have been updated to accept an optional `screenKey` parameter. This allows for the creation of more specific and unique transition keys by appending the screen key when provided.

--- app/src/main/java/com/example/connectify/utils/SharedTransition.kt
+++ app/src/main/java/com/example/connectify/utils/SharedTransition.kt
@@ -1,13 +1,20 @@
 package com.example.connectify.utils
 
 object SharedTransition {
-    fun sharedTransitionImageKey(id : String) : String {
-        return "shared_transition_image_key_$id"
-
+    fun sharedTransitionImageKey(id : String, screenKey: String? = null) : String {
+        return if (screenKey != null) {
+            "shared_transition_image_key_${screenKey}_$id"
+        } else {
+            "shared_transition_image_key_$id"
+        }
     }
 
-    fun sharedTransitionTitleKey(id : String) : String {
-        return "shared_transition_title_key_$id"
+    fun sharedTransitionTitleKey(id : String, screenKey: String? = null) : String {
+        return if (screenKey != null) {
+            "shared_transition_title_key_${screenKey}_$id"
+        } else {
+            "shared_transition_title_key_$id"
+        }
     }
 
 }
