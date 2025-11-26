6899c03bc9f250c452002084e54b88f1ee1799ce
byron-fran <1byron1999@gmail.com>
2025-11-26 05:52:07 -0800

    Create FavoriteContactsScreen to display favorited contacts

---
+++ app/src/main/java/com/example/connectify/presentation/screens/favorites/FavoriteContactsScreen.kt
@@ -0,0 +1,58 @@
+package com.example.connectify.presentation.screens.favorites
+
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.example.connectify.R
+import com.example.connectify.presentation.components.contact.ContactList
+import com.example.connectify.presentation.components.global.ConnectifyToAppBar
+import com.example.connectify.presentation.components.global.TitleMedium
+import com.example.connectify.presentation.navigation.Screens
+import com.example.connectify.presentation.screens.contact.ContactViewModel
+import com.example.connectify.ui.theme.Spacing
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+fun FavoriteContactsScreen(
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope,
+    contactViewModel: ContactViewModel = hiltViewModel<ContactViewModel>(),
+    onNavigateBack: () -> Unit,
+    onNavigateToDetail : (Screens) -> Unit
+) {
+
+    val favoritesContact = contactViewModel.contactState.collectAsState().value.contacts
+    
+    Scaffold(
+        topBar = {
+            ConnectifyToAppBar(
+                title = {
+                    TitleMedium(stringResource(R.string.favorites))
+                }
+            ) {
+                onNavigateBack()
+            }
+        }
+    ) { paddingValues ->
+        Column( modifier = Modifier.padding(paddingValues)) {
+            ContactList(
+                contacts = favoritesContact.filter { it.isFavorite },
+                sharedTransitionScope = sharedTransitionScope,
+                animatedContentScope = animatedContentScope,
+                modifier = Modifier.padding(horizontal = Spacing.spacing_sm),
+                enableSharedTransitions = true,
+                screenKey = "favorites"
+            ) {
+                onNavigateToDetail(Screens.ContactDetail(it, "favorites"))
+            }
+        }
+    }
+}
