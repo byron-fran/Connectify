fd67c71e0c04bb4d505027d47a63146adcae01f3
byron-fran <1byron1999@gmail.com>
2025-11-26 03:12:56 -0800

    Refactor MainActivity to use ContactNavigationSuiteScaffold
    The `NavHostManager` is now wrapped within a `ContactNavigationSuiteScaffold`. A `navController` is created and passed to both the scaffold for navigation handling and to the `NavHostManager`.

--- app/src/main/java/com/example/connectify/MainActivity.kt
+++ app/src/main/java/com/example/connectify/MainActivity.kt
@@ -4,18 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
+import com.example.connectify.presentation.navigation.ContactNavigationSuiteScaffold
 import com.example.connectify.presentation.navigation.NavHostManager
 import com.example.connectify.ui.theme.ConnectifyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+        val navController = rememberNavController()
             ConnectifyTheme {
-                NavHostManager()
+                ContactNavigationSuiteScaffold(
+                    onNavigateTo = {
+                        navController.navigate(it)
+                    }
+                ) {
+                   NavHostManager(navController)
+                }
             }
         }
     }
