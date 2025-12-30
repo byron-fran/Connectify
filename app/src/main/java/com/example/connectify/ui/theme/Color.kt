c4e9422456871e8baa791e7e383274ec7c7eedeb
byron-fran <1byron1999@gmail.com>
2025-12-29 19:53:33 -0800

    style: Update transparency for secondary and error colors
    Adjusted the alpha values for secondary and error colors in both light and dark themes to refine their visual appearance.
    
    - `DarkSecondary`: Changed alpha from `0x40` (~25%) to `0x26` (~15%).
    - `DarkError`: Changed alpha from `0x3B` (~23%) to `0x33` (~20%).
    - `LightError`: Changed alpha from `0x34` (~20%) to `0x26` (~15%).

--- app/src/main/java/com/example/connectify/ui/theme/Color.kt
+++ app/src/main/java/com/example/connectify/ui/theme/Color.kt
@@ -1,18 +1,17 @@
 package com.example.connectify.ui.theme
 
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 
 val DarkBackground = Color(0xFF000000)
 val DarkOnBackground = Color(0xFFFFFFFF)
 val DarkPrimary = Color(0xFF00bc7d)
 val DarkOnPrimary = Color(0xFFFFFFFF)
-val DarkSecondary = Color(0x4000BC7D)
+val DarkSecondary = Color(0x2600BC7D)
 val DarkOnSecondary = Color(0xFF00bc7d)
 val DarkTertiary = Color(0xFFffdf20)
 val DarkSurface = Color(0xFF2E2E2E)
 val DarkOnSurface = Color(0xFFD9D9D9)
-val DarkError = Color(0x3BFF0000)
+val DarkError = Color(0x33FF0000)
 val DarkOnError = Color(0xFFC51414)
 val LightBackground = Color(0xFFFFFFFF)
 val LightOnBackground = Color(0xFF000000)
@@ -23,5 +22,5 @@ val LightOnSecondary = Color(0xFF00bc7d)
 val LightTertiary = Color(0xFFffdf20)
 val LightSurface = Color (0xFFE9E8E8)
 val LightOnSurface = Color(0xFF2E2E2E)
-val LightError = Color(0x34F61A1A)
+val LightError = Color(0x26F61A1A)
 val LightOnError = Color(0xFFD91010)
