6a74c45a352f91c7592597bd4baba989a2f6b374
byron-fran <1byron1999@gmail.com>
2025-12-29 20:01:11 -0800

    feat: Integrate ThemeViewModel into MainActivity
    Updated `MainActivity` to retrieve the current theme mode from `ThemeViewModel`.
    - Obtained `ThemeViewModel` instance using `hiltViewModel()`.
    - Collected `themeMode` state from the ViewModel.
    - Passed `themeMode` to `ConnectifyTheme` to apply the selected theme preference dynamically.

--- app/src/main/java/com/example/connectify/MainActivity.kt
+++ app/src/main/java/com/example/connectify/MainActivity.kt
@@ -4,14 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
-import com.example.connectify.presentation.navigation.ContactNavigationSuiteScaffold
 import com.example.connectify.presentation.navigation.ConnectifyNavHost
+import com.example.connectify.presentation.navigation.ContactNavigationSuiteScaffold
 import com.example.connectify.presentation.navigation.navigateSingleBottomTo
 import com.example.connectify.presentation.navigation.navigationItems
+import com.example.connectify.presentation.screens.theme.ThemeViewModel
 import com.example.connectify.ui.theme.ConnectifyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,7 +29,10 @@ class MainActivity : ComponentActivity() {
             val navHostController = rememberNavController()
             val navBackStackEntry by navHostController.currentBackStackEntryAsState()
             val currentRoute = navBackStackEntry?.destination?.route
-            ConnectifyTheme {
+            val themeViewModel : ThemeViewModel = hiltViewModel<ThemeViewModel>()
+            val themeMode by themeViewModel.themeMode.collectAsState()
+
+            ConnectifyTheme(themeMode) {
                 ContactNavigationSuiteScaffold(
                     navigationItems = navigationItems,
                     currentRoute = currentRoute,
