1606da9300b8b966778e0d826ef698bf8316282f
Byron Francisco Chanax <1byron1999@gmail.com>
2026-01-02 10:09:09 -0800

    feat: Add delete all contacts capability to SettingsViewModel
    Updated `SettingsViewModel` to support the functionality for deleting all contacts.
    
    - Replaced `ThemeRepository` and `Application` context dependencies with `ContactUseCases`.
    - Added `deleteAllContacts` function which invokes the corresponding use case within the `viewModelScope`.

--- app/src/main/java/com/example/connectify/presentation/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/example/connectify/presentation/screens/settings/SettingsViewModel.kt
@@ -1,16 +1,23 @@
 package com.example.connectify.presentation.screens.settings
 
-import android.app.Application
 import androidx.lifecycle.ViewModel
-import com.example.connectify.domain.repositories.ThemeRepository
+import androidx.lifecycle.viewModelScope
+import com.example.connectify.domain.useCases.ContactUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
-    private val themeRepository: ThemeRepository,
-    private val context: Application
+    private val contactUseCases: ContactUseCases
 ) : ViewModel() {
 
+
+
+    fun deleteAllContacts() {
+        viewModelScope.launch {
+            contactUseCases.deleteAllContacts()
+        }
+    }
 
 }
